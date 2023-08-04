6a1015d4fd3ac82914eb1ae96afb931d54832e26
Bomi-kwon <bbomicoding@gmail.com>
2023-08-04 20:42:30 +0900

    sw 1989. 초심자의 회문검사 1

--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -1,6 +1,5 @@
 package algorithm_practice;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
@@ -8,46 +7,37 @@ public class Solution {
 	    Scanner sc = new Scanner(System.in);
 	    int N = sc.nextInt();
 	    
-	    ArrayList<Integer> numbers = new ArrayList<>();
+	    String[] words = new String[N];
+	    // 맨 첫줄에 주어진 숫자 길이만큼의 배열을 만들기
 	    
 	    for(int i = 0; i < N; i++) {
-	    	numbers.add(sc.nextInt());
+	    	words[i] = sc.next();
+	    	// 그 밑줄부터 적히는 단어들 배열에 다 넣기
 	    }
 	    
-	    // Collections.sort(numbers);
-	    // 아래부터는 예시를 보고 바꾼 더 간결한 선택 정렬 알고리즘
-	    
-	    int indexMin, temp;
-	    
-	    for(int l = 0; l < N-1; l++) {
-	    	indexMin = l;
+	    for(int i = 0; i < N; i++) {
+	    	// 배열 한칸씩 그 문자가 회문인지 알아보자.
 	    	
-		    for(int j = l+1 ; j < N; j++) {
-		    	
-		    	if(numbers.get(j) < numbers.get(indexMin)) {
-		    		
-		    		// 만약 indexMin번째 숫자보다 더 작은 숫자가 있다면?
-		    		indexMin = j; // indexMin을 그 숫자로 대체
-		    	}
-		    	// 더 작은 숫자 없으면 안 대체
-		    	// j 하나 늘려서 다시 반복
-		    }
-		    
-		    temp = numbers.get(indexMin); 
-		    // 그렇게 찾은 가장 작은 숫자를 temp에 집어넣기
-		    
-		    numbers.set(indexMin, numbers.get(l));
-		    // 가장 작은 숫자가 있던 자리에, 맨 앞에 있던 숫자 집어넣기
-		    numbers.set(l, temp);
-		    // 맨 앞에 가장 작은 숫자 집어넣기. 즉 가장 작은 숫자와 맨 앞 숫자를 swap
-		    
-		    // 한번 최솟값 찾아서 맨 앞에 넣고 정렬 끝나면,
-		    // 이제 맨 앞의 숫자는 무시하고 그 다음 칸부터 다시 똑같이 정렬하기
-		    // 반복문 앞에서 자연스럽게 그 다음 칸으로 넘어가게 됨
-		    // 그래서 최댓값보다는 최솟값을 기준으로 정렬하는게 나음.
+	    	int t_or_f = 1;
+	    	// 일단 회문 문자임을 가정하고 풀기 시작
+	    	
+	    	String[] alphabet = words[i].split("");
+	    	// 단어를 알파벳 단위로 쪼개 새로운 배열에 넣었음
+	    	
+	    	for(int j = 0; j < alphabet.length/2; j++) {
+	    		// 배열 길이가 홀수든 짝수든 2로 나눈 몫 만큼의 횟수로 비교하면 됨.
+	    		
+	    		if(!alphabet[j].equals(alphabet[alphabet.length-j-1])) {
+	    			t_or_f = 0;
+	    			break;
+	    			// 만약 마주보는 문자 비교하다가
+	    			// 서로 다른 문자 나오면 회문이 아니라고 체크한 후 바로 반복문 탈출.
+	    		}
+	    		
+	    		// if 구문에 걸리지 않았다면 회문 문자가 맞으므로 t_or_f 그대로 1로 두기.
+	    	}
+	    	System.out.printf("#%d %d\n", i+1, t_or_f);
 	    }
-	    
-	    System.out.println(numbers.get(N/2));
 	  }
 }
 
