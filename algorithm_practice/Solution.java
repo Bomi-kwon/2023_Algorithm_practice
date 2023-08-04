b761b7915a069602d5015a8fc62e4a77a5cbf631
Bomi-kwon <bbomicoding@gmail.com>
2023-08-04 22:01:53 +0900

    sw 1989. 초심자의 회문검사 5

--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -11,21 +11,16 @@ public class Solution {
 	    	String word = sc.next();
 	    	// 한줄에 적히는 단어 인식하기
 	    	
-	    	int t_or_f = 1;
-	    	// 일단 회문 문자임을 가정하고 풀기 시작
+	    	int t_or_f = 0;
+	    	// 일단 회문 문자가 아님을 가정하고 풀기 시작
 	    	
-	    	for(int j = 0; j < word.length()/2; j++) {
-	    		// 배열 길이가 홀수든 짝수든 2로 나눈 몫 만큼의 횟수로 비교하면 됨.
-	    		
-	    		if(word.charAt(j) != word.charAt(word.length()-j-1)) {
-	    			t_or_f = 0;
-	    			break;
-	    			// 마주보는 문자끼리 비교하다가
-	    			// 서로 다른 문자 나오면 회문이 아니라고 체크한 후 바로 반복문 탈출.
-	    		}
-	    		
-	    		// if 구문에 걸리지 않았다면 회문 문자가 맞으므로 t_or_f 그대로 1로 두기.
-	    	}
+	    	if(word.equals(new StringBuilder(word).reverse().toString())) {
+	    		// word와 똑같은 문자열을 생성한뒤, 뒤집고 문자열로 생성
+	    		// 출력하거나 String 변수에 넣을땐 toString()을 붙여야 함.
+	    		t_or_f = 1;
+	    		// 뒤집었는데 원래랑 똑같으면 회문 문자가 맞다.
+	    	} 
+	    	
 	    	System.out.printf("#%d %d\n", i+1, t_or_f);
 	    }
 	  }
