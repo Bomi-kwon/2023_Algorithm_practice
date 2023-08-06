52bfffcec6128aac2bc33e841ef5b2d8b0814f47
Bomi-kwon <bbomicoding@gmail.com>
2023-08-06 21:31:33 +0900

    sw 2005. 파스칼의 삼각형 3

--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -16,20 +16,21 @@ public class Solution {
 	    	// 몇번째 테스트 케이스인지 쓰기 
 
 	    	for(int j = 0 ; j < N ; j++) {
-	    		triangle[j][0] = 1;
-	    		triangle[j][j] = 1;
-	    		// 맨 윗줄 숫자는 무조건 1
-	    		// 두번째 줄 숫자도 1 1
-	    		// 몇번째 줄이던 맨 앞과 맨 뒤는 무조건 1
+	    		// j번째 줄에서..
 	    		
-	    		// 이제부턴 2번째 인덱스 줄부터 생각 (j번째 인덱스 줄이라고 생각)
-	    		for(int l = 1; l <= j-1; l++) {
-	    			triangle[j][l] = triangle[j-1][l-1] + triangle[j-1][l];
-	    			// j번째 줄의 l번째 값은 j-1번째 줄의 l-1번째값과 l번째 값의 합과 같음.
-	    		}
-	    		
-	    		for(int n = 0 ; n < j+1 ; n++) {
-	    			System.out.printf("%d ",triangle[j][n]);
+	    		for(int l = 0; l <= j; l++) {
+	    			// j번째 줄에는 숫자 j+1개 있음
+	    			
+	    			if(j == l || l == 0) {
+	    				triangle[j][l] = 1;
+	    				// 맨 윗줄 숫자는 무조건 1
+	    				// 두번째 줄 숫자도 1 1
+	    				// 몇번째 줄이던 맨 앞과 맨 뒤는 무조건 1
+	    			} else {
+	    				triangle[j][l] = triangle[j-1][l-1] + triangle[j-1][l];
+	    				// j번째 줄의 l번째 값은 j-1번째 줄의 l-1번째값과 l번째 값의 합과 같음.
+	    			}
+	    			System.out.printf("%d ",triangle[j][l]);
 	    			// 숫자 적고 오른쪽 한칸 띄어쓰기
 	    		}
 	    		System.out.println("");
