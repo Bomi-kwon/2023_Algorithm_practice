e134d64a9a0a54a5b690f55c1e83a60044e1d421
Bomi-kwon <bbomicoding@gmail.com>
2023-08-11 13:05:41 +0900

    sw 1961. 숫자 배열 회전 3

--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -11,10 +11,7 @@ public class Solution {
 	    for(int i = 0; i < T; i++) {
 	    	int N = sc.nextInt();
 	    	int[][] cube = new int[N][N];
-	    	int[][] degree_90 = new int[N][N];
-	    	int[][] degree_180 = new int[N][N];
-	    	int[][] degree_270 = new int[N][N];
-	    	// 주어진 것과 돌릴 것들을 담을 2차원 배열 만들기 (다 정사각형 배열이고 숫자 들어감)
+	    	// 주어진 것을 담을 2차원 배열 만들기 (정사각형 배열이고 숫자 들어감)
 	    	
 	    	for(int row = 0 ; row < N ; row++) {
 	    		// row번째 행
@@ -24,28 +21,9 @@ public class Solution {
 	    		}
 	    	}
 	    	
-	    	for(int row = 0 ; row < N ; row++) {
-	    		for(int column = 0 ; column < N ; column++) {
-	    			degree_90[row][column] = cube[N-column-1][row];
-	    			// 90도 회전하면?
-	    			// 행과 열이 바뀌고 row는 숫자가 거꾸로 감소
-	    		}
-	    	}
-	    	
-	    	for(int row = 0 ; row < N ; row++) {
-	    		for(int column = 0 ; column < N ; column++) {
-	    			degree_180[row][column] = degree_90[N-column-1][row];
-	    			// 한번더 회전해서 180도 배열에 넣기
-	    		}
-	    	}
-
-	    	for(int row = 0 ; row < N ; row++) {
-	    		for(int column = 0 ; column < N ; column++) {
-	    			degree_270[row][column] = degree_180[N-column-1][row];
-	    			// 한번더 회전해서 270도 배열에 넣기
-	    		}
-	    	}
-	    	
+	    	int[][] degree_90 = Rotation(cube);
+	    	int[][] degree_180 = Rotation(degree_90);
+	    	int[][] degree_270 = Rotation(degree_180);
 
 	    	System.out.printf("#%d\n",i+1);
 	    	// 몇번째 테스트 케이스인지 출력
@@ -69,5 +47,18 @@ public class Solution {
 	    	
 	    }
 	  }
-}
 
+	private static int[][] Rotation(int[][] cube) {
+		int[][] rotated_cube = new int[cube.length][cube.length];
+		
+		for(int row = 0 ; row < cube.length ; row++) {
+    		for(int column = 0 ; column < cube.length ; column++) {
+    			rotated_cube[row][column] = cube[cube.length-column-1][row];
+    			// 90도 회전하면?
+    			// 행과 열이 바뀌고 row는 숫자가 거꾸로 감소
+    		}
+    	}
+		
+		return rotated_cube;
+	}
+}
