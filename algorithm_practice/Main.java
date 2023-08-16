55f5ed72cfddd9caaced8851d854d074c610c9f4
Bomi-kwon <bbomicoding@gmail.com>
2023-08-16 13:34:30 +0900

    pg. 피보나치 수

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -1,63 +1,9 @@
 package algorithm_practice;
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-    	int[][] cube = new int[N][N];
-    	int[][] degree_90 = new int[N][N];
-    	int[][] degree_180 = new int[N][N];
-    	int[][] degree_270 = new int[N][N];
-    	
-    	for(int row = 0 ; row < N ; row++) {
-    		// row번째 행
-    		for(int column = 0 ; column < N ; column++) {
-    			cube[row][column] = sc.nextInt();
-    			// 주어진 숫자들 2차원 배열에 넣기
-    		}
-    	}
-    	
-    	for(int row = 0 ; row < N ; row++) {
-    		for(int column = 0 ; column < N ; column++) {
-    			degree_90[row][column] = cube[N-column-1][row];
-    			// 90도 회전하면?
-    			// 행과 열이 바뀌고 row는 숫자가 거꾸로 감소
-    		}
-    	}
-    	
-    	for(int row = 0 ; row < N ; row++) {
-    		for(int column = 0 ; column < N ; column++) {
-    			degree_180[row][column] = degree_90[N-column-1][row];
-    			// 한번더 회전해서 180도 배열에 넣기
-    		}
-    	}
-
-    	for(int row = 0 ; row < N ; row++) {
-    		for(int column = 0 ; column < N ; column++) {
-    			degree_270[row][column] = degree_180[N-column-1][row];
-    			// 한번더 회전해서 270도 배열에 넣기
-    		}
-    	}
-    	
-    	for(int row = 0 ; row < N ; row++) {
-    		for(int column = 0 ; column < N ; column++) {
-    			System.out.print(degree_90[row][column]);
-    		}
-    		System.out.print(" ");
-    		
-    		for(int column = 0 ; column < N ; column++) {
-    			System.out.print(degree_180[row][column]);
-    		}
-    		System.out.print(" ");
-    		
-    		for(int column = 0 ; column < N ; column++) {
-    			System.out.print(degree_270[row][column]);
-    		}
-    		System.out.println();
-    	}
-    	
-    	
+		
+		System.out.println(1234567 % 1234567);
+		
 	  }
 }
--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -5,47 +5,20 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		// 테스트 케이스는 총 T개 일 것이다.
+		int n = sc.nextInt();
 		
-		for(int i = 0; i < T; i++) {
-			int N = sc.nextInt();
-			// Command 갯수는 총 N개이다.
-			
-			int speed = 0;
-			// 초기 속도는 0이다.
-			
-			int distance = 0;
-			// 초기 이동거리도 0이다.
-			
-			for(int j = 0; j< N; j++) {
-				
-				int p_or_m = sc.nextInt();
-				// 가속인지 감속인지 구분
-				
-				if(p_or_m != 0) {
-					// 0이면 다음 숫자 없음. 가속/감속이면 얼만큼 할건지 뒤에 숫자 있음
-					int how_much = sc.nextInt();
-					
-					if(p_or_m == 1) {
-						speed += how_much;
-						// 가속이면 뒤 숫자만큼 속도 커짐
-					} else if(p_or_m == 2) {
-						speed -= how_much;
-						// 감속이면 뒤 숫자만큼 속도 작아짐
-						if(speed < 0) {
-							// 그렇게 계산을 했는데 속도가 마이너스가 될 경우
-							// 그냥 속도 0으로 계산
-							speed = 0;
-						}
-					}
-				}
-				
-				distance += speed;
-				// 이전 command까지 이동했던 거리에 새롭게 이동한 거리 추가해주기
-				// 1초동안 이동하는 거라 speed가 곧 이동거리를 의미
-			}
-			System.out.printf("#%d %d\n", i+1, distance);
+		long[] arr = new long[n+1];
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		for(int i = 2 ; i <= n ; i++) {
+			arr[i] = (arr[i-2] + arr[i-1])%1234567;
 		}
-	}
+		
+		for(int i = 0 ; i <= n ; i++) {
+			System.out.printf("arr[%d] : %d\n", i, arr[i]);
+			System.out.printf("arr[%d]를 1234567로 나눈 나머지 : %d\n", i, arr[i]%1234567);
+		}
+		
+ 	}
 }
