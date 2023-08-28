cad8ac72b20a5f29194e1080810ac0844c92e4b9
Bomi-kwon <bbomicoding@gmail.com>
2023-08-28 10:31:09 +0900

    9-1. 씨름선수

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -1,9 +1,46 @@
 package algorithm_practice;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(1234567 % 1234567);
+	    int N = sc.nextInt();
+	    int[][] ath = new int[N][2];
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	    	ath[i][0] = sc.nextInt();
+	    	ath[i][1] = sc.nextInt();
+	    }
+	    
+	    int athNum = N;
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	    	// i는 기준으로 할 사람의 인덱스임
+	    	
+	    	for(int j = 0 ; j < N ; j++) {
+	    		// j는 비교할 사람의 인덱스임
+	    		
+	    		if(ath[i][0] < ath[j][0]) {
+	    			// 내가 키가 작다면?
+	    			
+	    			if(ath[i][1] < ath[j][1]) {
+	    				// 키도 작고 몸무게도 작으면 나 탈락(비교 끝)
+	    				athNum--;
+	    				break;
+	    			} else {
+	    				// 키는 작은데 몸무게는 크면 합격
+	    				continue;
+	    			}
+	    		} else {
+	    			// 내가 키 크면 그냥 넘어감
+	    			continue;
+	    		}
+	    	}
+	    }
+	    
+	    System.out.println(athNum);
 		
 	  }
 }
--- algorithm_practice/Solution.java
+++ algorithm_practice/Solution.java
@@ -1,24 +1,49 @@
 package algorithm_practice;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
 		
-		long[] arr = new long[n+1];
-		arr[0] = 0;
-		arr[1] = 1;
+		int people[] = {70, 80, 50, 60, 50, 30};
+		// collections 함수를 사용하려면 int 안되고 Integer여야함
 		
-		for(int i = 2 ; i <= n ; i++) {
-			arr[i] = (arr[i-2] + arr[i-1])%1234567;
+		Integer weight[] = Arrays.stream(people).boxed().toArray(Integer[]::new);
+		// 그래서 int배열을 Integer 배열로 바꾸기
+		
+		int limit = 100;
+		// 보트 하나당 무게 제한
+		
+		int boatNum = 0;
+		// 구명보트 갯수 0으로 시작
+		
+		Arrays.sort(weight, Collections.reverseOrder());
+		// 몸무게 내림차순으로 재배열
+		
+		System.out.println("몸무게순으로 내림차순한 배열 : " + Arrays.toString(weight));
+		
+		int end = weight.length - 1;
+		// 몸무게 제일 작은애 인덱스
+		
+		for(int start = 0 ; start < weight.length ; start++) {
+			
+			if(weight[start] + weight[end] <= limit) {
+				boatNum++;
+				end--;
+				// 제일 무거운애랑 제일 가벼운애 더해서 리밋 안 넘으면
+				// 보트에 넣고 맨 뒤 인덱스도 한칸 앞으로
+			} else {
+				boatNum++;
+				// 근데 만약 둘이 더해서 리밋 넘으면
+				// 맨 뒤 인덱스 유지. 맨 앞 인덱스는 반복문 때문에 자동 증가
+			}
+			
+			if(start >= end) {
+				break;
+				// 앞의 인덱스가 뒤의 인덱스보다 크거나 같으면 보트에 다 탄 것
+			}
 		}
-		
-		for(int i = 0 ; i <= n ; i++) {
-			System.out.printf("arr[%d] : %d\n", i, arr[i]);
-			System.out.printf("arr[%d]를 1234567로 나눈 나머지 : %d\n", i, arr[i]%1234567);
-		}
-		
- 	}
+		System.out.println(boatNum);
+  }
 }
