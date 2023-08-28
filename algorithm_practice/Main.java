844cbc3d7669b06f71d68a1db3baae2662c01cb7
Bomi-kwon <bbomicoding@gmail.com>
2023-08-28 12:02:03 +0900

    9-2. 회의실 배정

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -9,48 +9,43 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 	    int N = sc.nextInt();
-	    int[][] ath = new int[N][2];
+	    int[][] meeting = new int[N][2];
 	    
 	    for(int i = 0 ; i < N ; i++) {
-	    	ath[i][0] = sc.nextInt();
-	    	ath[i][1] = sc.nextInt();
+	    	meeting[i][0] = sc.nextInt();
+	    	meeting[i][1] = sc.nextInt();
 	    }
 	    
-	    int athNum = 0;
+	    int meetingNum = 0;
 	    
-	    Arrays.sort(ath, new Comparator<int[]>() {
-	    	// 선수들 키 큰 순서대로 나열하기!!
+	    Arrays.sort(meeting, new Comparator<int[]>() {
+	    	// 회의가 먼저 끝나는 순으로 정렬하기!!
 	    	
 	    	@Override
 	    	public int compare(int[] o1, int[] o2) {
-	    		if(o1[0] != o2[0]) {
-	    			return o2[0] - o1[0];
-	    			// 키 기준 내림차순
+	    		if(o1[1] != o2[1]) {
+	    			return o1[1] - o2[1];
+	    			// 회의 끝나는 시간 기준 오름차순
 	    		} else {
-	    			return o2[1] - o1[1];
-	    			// 키 같으면 몸무게 기준 내림차순
+	    			return o1[0] - o2[0];
+	    			// 회의 끝시간 같으면 시작시간 기준 오름차순
 	    		}
 	    	}
 		});
 	    
-	    int maxWeight = ath[0][1];
-	    // 몸무게 최댓값에 키 제일 큰 사람 몸무게 넣어놓기
-	    athNum++;
-	    // 키 제일 큰 사람은 무조건 선수로 발탁되야함
+	    int frontEnd = meeting[0][1];
+	    // 앞 회의 끝난 시간에 가장 먼저 끝난 회의 끝시간 넣어놓기
+	    meetingNum++;
+	    // 제일 먼저 끝나는 회의는 무조건 해야함
 	    
 	    for(int i = 1 ; i < N ; i++) {
-	    	if(ath[i][1] >= maxWeight) {
-	    		maxWeight = ath[i][1];
-	    		// 만약 비교대상의 몸무게가 최댓값보다 크면 그걸로 최댓값 바꾸고
-	    		athNum++;
-	    		// 선수 한명 뽑혔으므로 추가해주기
-	    	} else {
-	    		continue;
-	    		// 만약 몸무게가 최댓값보다 작으면 탈락. 최댓값 안 바뀌고 선수로도 안 뽑힘
+	    	if(meeting[i][0] >= frontEnd) {
+	    		frontEnd = meeting[i][1];
+	    		// 만약 다음 회의의 시작시간이 앞회의 끝시간보다 뒤면 그 회의의 끝시간을 앞회의 끝시간으로 바꾸고
+	    		meetingNum++;
+	    		// 회의 하나 더 추가해주기
 	    	}
 	    }
-	    
-	    System.out.println(athNum);
-		
+	    System.out.println(meetingNum);
 	  }
 }
