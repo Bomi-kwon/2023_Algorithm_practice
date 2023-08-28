a865259aa318d5cc46ab75ec9cf15e66ccd2038e
Bomi-kwon <bbomicoding@gmail.com>
2023-08-28 11:14:32 +0900

    9-1. 씨름선수 2

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -1,5 +1,7 @@
 package algorithm_practice;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -14,29 +16,37 @@ public class Main {
 	    	ath[i][1] = sc.nextInt();
 	    }
 	    
-	    int athNum = N;
+	    int athNum = 0;
 	    
-	    for(int i = 0 ; i < N ; i++) {
-	    	// i는 기준으로 할 사람의 인덱스임
+	    Arrays.sort(ath, new Comparator<int[]>() {
+	    	// 선수들 키 큰 순서대로 나열하기!!
 	    	
-	    	for(int j = 0 ; j < N ; j++) {
-	    		// j는 비교할 사람의 인덱스임
-	    		
-	    		if(ath[i][0] < ath[j][0]) {
-	    			// 내가 키가 작다면?
-	    			
-	    			if(ath[i][1] < ath[j][1]) {
-	    				// 키도 작고 몸무게도 작으면 나 탈락(비교 끝)
-	    				athNum--;
-	    				break;
-	    			} else {
-	    				// 키는 작은데 몸무게는 크면 합격
-	    				continue;
-	    			}
+	    	@Override
+	    	public int compare(int[] o1, int[] o2) {
+	    		if(o1[0] != o2[0]) {
+	    			return o2[0] - o1[0];
+	    			// 키 기준 내림차순
 	    		} else {
-	    			// 내가 키 크면 그냥 넘어감
-	    			continue;
+	    			return o2[1] - o1[1];
+	    			// 키 같으면 몸무게 기준 내림차순
 	    		}
+	    	}
+		});
+	    
+	    int maxWeight = ath[0][1];
+	    // 몸무게 최댓값에 키 제일 큰 사람 몸무게 넣어놓기
+	    athNum++;
+	    // 키 제일 큰 사람은 무조건 선수로 발탁되야함
+	    
+	    for(int i = 1 ; i < N ; i++) {
+	    	if(ath[i][1] >= maxWeight) {
+	    		maxWeight = ath[i][1];
+	    		// 만약 비교대상의 몸무게가 최댓값보다 크면 그걸로 최댓값 바꾸고
+	    		athNum++;
+	    		// 선수 한명 뽑혔으므로 추가해주기
+	    	} else {
+	    		continue;
+	    		// 만약 몸무게가 최댓값보다 작으면 탈락. 최댓값 안 바뀌고 선수로도 안 뽑힘
 	    	}
 	    }
 	    
