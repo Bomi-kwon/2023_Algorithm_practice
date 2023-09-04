063418af72d1d66b5367041fae607f9332d985c7
Bomi-kwon <bbomicoding@gmail.com>
2023-09-04 23:08:39 +0900

    1-2. 대소문자 변환 2

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -11,24 +11,16 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 	    String str = sc.nextLine();
 	    
-	    List<Character> arr = new ArrayList<>(); // 변환한 char 담을 리스트
+	    String answer = "";
 	    
 	    for(char x : str.toCharArray()) {
 	    	if(Character.isUpperCase(x)) {
-	    		x = Character.toLowerCase(x);
+	    		answer += Character.toLowerCase(x);
 	    	} else {
-	    		x = Character.toUpperCase(x);
+	    		answer += Character.toUpperCase(x);
 	    	}
-	    	arr.add(x);
-	    	// 변환한 뒤 리스트에 담기
 	    }
 	    
-	    StringBuilder sb = new StringBuilder(); // 스트링빌더 이용
-	    
-	    for(Character ch : arr) {
-	    	sb.append(ch); // 리스트 속 char를 하나의 문자열로 엮기
-	    }
-	    
-	    System.out.println(sb.toString());
+	    System.out.println(answer);
 	}
 }
