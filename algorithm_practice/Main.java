46199bbfc8d8bade083bd87010788f921469ef45
Bomi-kwon <bbomicoding@gmail.com>
2023-09-05 23:05:20 +0900

    2-1. 큰 수 출력하기

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -7,20 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-	    String str = sc.nextLine();
-	    
-	    String answer = "";
-	    
-	    for(char x : str.toCharArray()) {
-	    	if(x >= 65 && x <= 90) {
-	    		answer += (char)(x+32);
-	    		// 65~90 사이면 대문자
-	    		// 대문자에 32를 더하면 소문자됨
-	    		// 숫자를 다시 문자로 형변환 해줘야됨
-	    	} else {
-	    		answer += (char)(x-32);
-	    	}
-	    }
-	    System.out.println(answer);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0 ; i < N ; i++) {
+			if(i == 0) {
+				System.out.print(arr[i] + " ");
+			} else {
+				if(arr[i-1] < arr[i]) {
+					System.out.print(arr[i] + " ");
+				}
+			}
+		}
+		
+		
 	}
 }
