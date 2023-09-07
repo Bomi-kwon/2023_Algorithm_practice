40a9b6d4e32f7147b2f41b3408e7f3e220cb7d98
Bomi-kwon <bbomicoding@gmail.com>
2023-09-07 17:01:03 +0900

    3-2. 공통원소 구하기

--- algorithm_practice/Main.java
+++ algorithm_practice/Main.java
@@ -1,6 +1,7 @@
 package algorithm_practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -24,30 +25,33 @@ public class Main {
 			b[i] = sc.nextInt(); // b배열 따로
 		}
 		
+		Arrays.sort(a);
+		Arrays.sort(b);
+		// a배열과 b배열 모두 오름차순으로 정렬
+		
 		int p1 = 0, p2 = 0; // 포인터 두개 만들어주기
 		// 포인터들은 각 배열의 인덱스를 가리킴.
+		
 		List<Integer> answer = new ArrayList<>(); // 정답리스트
 		
 		while(p1 < N && p2 < M) {
 			if(a[p1] < b[p2]) {
+				p1++;
+				// 그럼 a[p1]은 절대 똑같은 값이 나올리가 없으므로
+				// p1은 그 다음을 가리키기
+			} else if(a[p1] == b[p2]){
 				answer.add(a[p1]);
 				p1++;
-				// 위 두 줄은 answer.add(a[p1++])로 합칠수 있음.
-				// p1++은 p1값을 넣은 다음 1증가 시키겠다는 소리임.
-				// 후위연산임!!
-			} else {
-				answer.add(b[p2]);
 				p2++;
-			} // p1 == N이거나 p2 == M이면 둘중 한 배열을 다 쓴것!
-		}
-
-		while(p1 < N) {
-			answer.add(a[p1++]); // 여기선 합쳐서 써봤음
-			// 만약 a 배열이 아직 안 끝났으면 나머지 쭈루룩 다 넣기
-		}
-		while(p2 < M) {
-			answer.add(b[p2++]);
-			// 만약 b 배열이 아직 안 끝났으면 나머지 쭈루룩 다 넣기
+				// 둘이 똑같으면 정답 리스트에 넣고
+				// p1이랑 p2둘다 다음걸로 넘어가서 비교하기
+			} else {
+				p2++;
+				// 그럼 b[p2]는 절대 똑같은 값이 나올리가 없으므로
+				// p2는 그 다음을 가리키기
+			}
+			// p1 == N이거나 p2 == M이면 둘중 한 배열을 다 쓴것!
+			// 그럼 똑같은 수가 절대 없다는 것이므로 여기서 반복문 끝.
 		}
 		
 		for(int x : answer) {
